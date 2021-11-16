df7fc85f6309988a1067deafbc907ff612ab3b6c
Trevor Leibert <tleibert1999@gmail.com>
2021-11-15 20:24:44 -0500

    Render data into a template with a salesforce style sheet

--- hello/views.py
+++ hello/views.py
@@ -6,47 +6,17 @@ from simple_salesforce import Salesforce
 
 from .models import Greeting
 
-# Create your views here.
+
+# Render data from simple_salesforce into a template using a salesforce style sheet
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    # return render(request, "index.html")
+
     sf = Salesforce(
         username=os.getenv("SF_USERNAME"),
         password=os.getenv("SF_PASSWORD"),
         security_token=os.getenv("SF_TOKEN"),
     )
-    sf_data = sf.query_all("SELECT Name, Phone FROM Contact")
-    # r = requests.get('http://httpbin.org/status/418')
-    # print(r.text)
-
-    rows = [
-        f"""<tr>
-    <td>
-    {entry['Name']}
-    </td>
-    <td>
-    {entry['Phone']}
-    </td>
-    </tr>
-    """
-        for entry in sf_data["records"]
-    ]
-    resp = """<h>Trevor's Salesforce Contacts</h>
-    <p>This data is pulled straight from salesforce through simple_salesforce!</p>
-    <p>It's rendered through python f-strings right now, until I get the hang of django templating.</p>
-    </br>
-    <table>
-    <tr>
-        <th>Name</th>
-        <th>Phone</th>
-    </tr>
-    {rows}
-    </table>
-    """.format(
-        rows="\n".join(rows)
-    )
-
-    return HttpResponse(resp)
+    sf_data = sf.query_all("SELECT Name, Title, Email, Phone FROM Contact")
+    return render(request, "table.html", context=sf_data)
 
 
 def db(request):
